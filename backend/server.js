74a3d27157e4206b3cc1f4c79da7f624e7ac320e
Usmonkhon Latipov <latipovusmonkhon2006@gmail.com>
2023-11-14 01:02:02 +0500

    Move functions to separate controller file

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ dotenv.config();
 import express from "express";
 import connectDb from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 connectDb();
 
@@ -10,9 +11,12 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("API IS RUNNING...");
+  res.send("API IS RUNNING...");
 });
 
 app.use("/api/products", productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`server is running on localhost:${port}`));
