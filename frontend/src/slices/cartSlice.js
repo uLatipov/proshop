7c477309c06f92b750c3ab4c9d8534bef3823490
Usmonkhon Latipov <latipovusmonkhon2006@gmail.com>
2023-11-20 21:19:48 +0500

    some changes

---
+++ frontend/src/slices/cartSlice.js
@@ -0,0 +1,54 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = localStorage.getItem("cart")
+    ? JSON.parse(localStorage.getItem("cart"))
+    : [];
+
+const addDecimals = (number) => {
+    return (Math.round(number * 100) / 100).toFixed();
+};
+
+const cartSlice = createSlice({
+    name: "cart",
+    initialState,
+    reducers: {
+        addToCart: (state, action) => {
+            const item = action.payload;
+
+            const existItem = state.cartItems.find((x) => x._id === item._id);
+
+            if (existItem) {
+                state.cartItems = state.cartItems.map((x) =>
+                    x._id === existItem._id ? item : x
+                );
+            } else {
+                state.cartItems = [...state.cartItems, item];
+            }
+
+            // calculate items price
+            state.itemsPrice = addDecimals(
+                state.cartItems.reduce(
+                    (acc, item) => acc + item.price * item.qty,
+                    0
+                )
+            );
+            // calculate shipping price (if order costs 100+$ free, otherwise 10$)
+            state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
+            // calculate tax price (15% tax)
+            state.taxPrice = addDecimals(
+                Number((0.15 * state.itemsPrice).toFixed(2))
+            );
+            // calculate total price
+            state.totalPrice = (
+                Number(state.itemsPrice) +
+                Number(state.shippingPrice) +
+                Number(state.taxPrice)
+            ).toFixed(2);
+
+            localStorage.setItem("cart", JSON.stringify(state));
+        },
+    },
+});
+
+export const { addToCart } = cartSlice.actions;
+export default cartSlice.reducer;
