1f54080e971c3920aa003712789818d805e0d0d7
Usmonkhon Latipov <latipovusmonkhon2006@gmail.com>
2023-11-22 04:20:08 +0500

    some changes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,6 +5,7 @@ import User from "../models/userModel.js";
 // @route  POST /api/users/login
 // @access Public
 const authUser = asyncHandler(async (req, res) => {
+  console.log(req.body);
   res.send("auth user");
 });
 
