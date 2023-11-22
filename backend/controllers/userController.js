1d5a5d4c003ac39d4628b4e003c28e250440d7a6
Usmonkhon Latipov <latipovusmonkhon2006@gmail.com>
2023-11-22 13:23:10 +0500

    add logout functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -46,7 +46,10 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route  POST /api/users/logout
 // @access Private
 const logoutUser = asyncHandler(async (req, res) => {
-  res.send("logout user");
+  res.clearCookie("jwt");
+  res.status(200).json({
+    message: "Logged out successfully",
+  });
 });
 
 // @desc   Get user profile
