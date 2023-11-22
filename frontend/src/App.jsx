abec96fa5a5daa4542a6219fc092e0a4823b72e8
Usmonkhon Latipov <latipovusmonkhon2006@gmail.com>
2023-11-22 16:32:49 +0500

    implement login & logout functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,23 @@
 import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { Outlet } from "react-router-dom";
+
 const App = () => {
-    return (
-        <>
-            <Header />
-            <main className='py-3'>
-                <Container>
-                    <Outlet />
-                </Container>
-            </main>
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+      <ToastContainer />
+    </>
+  );
 };
 
 export default App;
