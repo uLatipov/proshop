71288b42a5f73235909d6ea03a3f01aa344990b2
Usmonkhon Latipov <latipovusmonkhon2006@gmail.com>
2023-11-23 00:09:13 +0500

    create order routes endpoints

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import {
+  addOrderItems,
+  getMyOrders,
+  getOrderById,
+  updateOrderToPaid,
+  updateOrderToDelivered,
+  getOrders,
+} from "../controllers/orderController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
+
+const router = Router();
+
+router.route("/").post(protect, addOrderItems).get(protect, admin, getOrders);
+router.get("/mine", protect, getMyOrders);
+router.put("/:id/deliver", protect, admin, updateOrderToDelivered);
+router.route("/:id/pay").put(protect, updateOrderToPaid);
+router.get("/:id", protect, admin, getOrderById);
+
+export default router;
